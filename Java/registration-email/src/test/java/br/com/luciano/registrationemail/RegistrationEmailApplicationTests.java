ce5c3304ac9815bbbcfbe28d3b384f5f5a727d44
Luciano <luc.oliveira343@gmail.com>
2023-08-29 22:29:14 -0300

    adicionado projeto registration-email

---
+++ Java/registration-email/src/test/java/br/com/luciano/registrationemail/RegistrationEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.luciano.registrationemail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
